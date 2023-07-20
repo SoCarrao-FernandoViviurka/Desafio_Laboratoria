ce844cc3c33473ab876d7ce3046fd18e1d999863
AdrianaKatarina <dricakatarina@gmail.com>
2023-07-20 17:07:06 -0300

    style(Card): css responsive

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import './App.css';
+import { React, useEffect, useState } from "react";
+import data from './data/data.json';
+import Button from './Components/Button/Button';
+import Card from './Components/Card/Card';
+
+const App = () => {
+  const [cardPerPage, setCardPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [isCardVertical, setIsCardVertical] = useState(true);
+
+  const numberOfPages = Math.ceil(data.length / cardPerPage);
+  const startIndex = currentPage * cardPerPage;
+  const endIndex = startIndex + cardPerPage;
+  const currentCard = data.slice(startIndex, endIndex);
+
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [setCardPerPage])
+
+  const handleClickLayoutChange = () => {
+    setIsCardVertical(!isCardVertical);
+  }
+  
+  const modelFormatting = (model) => {
+    const lowercase = model.toLowerCase();
+    const separateString = lowercase.split(' ');
+    const capitalFirstLetter = separateString.map((letter) => {
+      return letter[0].toUpperCase() + letter.substring(1);
+    });
+    const removeRepeats = new Set(capitalFirstLetter);
+    // console.log(removeRepeats);
+    //console.log((Array.from(removeRepeats).join(' ')).length)
+    const transformToString = Array.from(removeRepeats).join(' ');    
+    return transformToString;
+  }
+
+  return (
+    <main className='Geral'>
+      <section className='Filters'>
+        <h5>Visualização:</h5>
+        <select className='Select-quantity' value={cardPerPage} onChange={(e) => setCardPerPage(Number(e.target.value))}>
+          {[5,10,20,50].map((value) => (
+            <option key={value} value={value}>{`${value} por página`}</option>
+          ))}
+        </select>
+        <Button className='Bg-orange' onClick={handleClickLayoutChange} children={isCardVertical ? 'Horizontal' : 'Vertical'}/>
+      </section>
+      <Card 
+        isCardVertical={isCardVertical}
+        vehicles={currentCard}
+        modelFormatting={modelFormatting}
+      />
+      <section>
+        {Array.from(Array(numberOfPages), (_, i) => {
+          return <Button className='Button-pages' key={i} value={i} onClick={(e) => setCurrentPage(Number(e.target.value))} children={i+1} />
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,27 @@
+import './Card.css';
+import Button from '../Button/Button';
+
+const Card = ({
+  isCardVertical,
+  vehicles,
+  modelFormatting,
+}) => {
+  return <div className='Position-cards'>
+    {vehicles.map((vehicle) => (
+      <section key={vehicle.veiculo_id} className={isCardVertical ? 'Vertical-card' : 'Horizontal-card'}>
+        <img className='Photo-card' src={vehicle.veiculo_foto[0]} alt={`foto do veiculo ${vehicle.veiculo_marca}`} />
+        <div className='Position'>
+          <p className='City'>{vehicle.veiculo_cidade}</p>
+          <h4 className='Brand'>{vehicle.veiculo_marca} {vehicle.modelo_nome_pai}</h4>
+          <p className='Elipse'>{modelFormatting(vehicle.veiculo_modelo)}</p>
+          <h3 className='Price'>{Number(vehicle.veiculo_valor).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h3>
+          <p className='Information'>{Number(vehicle.veiculo_km).toLocaleString()}km | {vehicle.veiculo_cambio} | {vehicle.ano_modelo}</p>          
+          <Button className='Btn-card Financing' children='Simular Financiamento' />
+          <Button className='Btn-card Contact' children='Entrar em contato' />
+        </div>
+      </section>
+    ))}
+  </div>
+}
+
+export default Card;
